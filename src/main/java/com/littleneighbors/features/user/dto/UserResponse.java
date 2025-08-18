9138caaa646f9e7540f56e8dc4cc4c658315e3a1
Alexandracoder <acasaviajes@gmail.com>
2025-08-18 21:41:43 +0200

    chore: blank spaces out

--- src/main/java/com/littleneighbors/features/user/dto/UserResponse.java
+++ src/main/java/com/littleneighbors/features/user/dto/UserResponse.java
@@ -9,7 +9,10 @@ public class UserResponse {
     private Long id;
     private String email;
     private String role;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
